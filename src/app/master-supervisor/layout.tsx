0c8b2bf38ad29092b17ed8e287b384bf82342601
Health PROMO <msf.hp.mbare@gmail.com>
2026-02-17 13:04:10 +0000

    this should be pinned at the top of the page within the /master-supervis

--- src/app/master-supervisor/layout.tsx
+++ src/app/master-supervisor/layout.tsx
@@ -56,12 +56,12 @@ export default function MasterSupervisorLayout({
           </SidebarContent>
           <SidebarFooter />
         </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="flex flex-col sm:gap-4 sm:pl-14">
           <DashboardHeader 
             title="Master Supervisor" 
             user={{ name: 'Admin User', avatarId: 'master-supervisor-avatar' }}
           />
-          <main className="flex-1 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <main className="flex-1 p-4 sm:px-6 sm:py-4 md:gap-8">
             {children}
           </main>
         </div>
